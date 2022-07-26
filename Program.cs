46de96523f0b8a544ec5e7b92dbc04fdfc2fae33
Ashutosh Gupta <ashutosh.gupta@sciex.com>
2022-07-26 18:28:20 +0530

    projects

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdvanceNetConcepts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

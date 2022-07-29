3f877c6f4521636833938272c2e1c92bbc729110
Gervel Giva <gervelgiva@gmail.com>
2022-07-29 08:45:04 +0800

    Assignment 1

--- Gervel/NetConcepts.DynamicProgramming/Program.cs
+++ Gervel/NetConcepts.DynamicProgramming/Program.cs
@@ -9,10 +9,43 @@ namespace NetConcepts.DynamicProgramming
     {
         static void Main(string[] args)
         {
+            _ = args;
+            var bindingFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic;
+
+            var assembly = Assembly.LoadFile(@"C:\Source\Workspace\Training\NetCom\Gervel\NetConcepts.Model\bin\Debug\net5.0\NetConcepts.Model.dll");
+
+            var trainingType = assembly.GetType("NetConcepts.Model.Models.Training");
+
+            Print(trainingType.Name.ToString(), "'Training' Type");
+
+            var members = trainingType.GetMembers(bindingFlags);
+            Print(members, "Members");
+
+            var methods = trainingType.GetMethods(bindingFlags);
+            Print(methods, "Methods");
+
+            var properties = trainingType.GetProperties(bindingFlags);
+            Print(properties, "Properties");
 
             Console.ReadLine();
         }
 
-      
+        private static void Print(string content, string label) {
+            Console.WriteLine($"{ label }:\n");
+
+            Console.WriteLine(content);
+
+            Console.WriteLine("\n********\n");
+        }
+
+        private static void Print(MemberInfo[] companyFields, string label) {
+            Console.WriteLine($"{ label }:\n");
+
+            foreach (var item in companyFields) {
+                Console.WriteLine($"\t{ item.Name }");
+            }
+
+            Console.WriteLine("\n********\n");
+        }
     }
 }

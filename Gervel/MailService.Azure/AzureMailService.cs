4666478aa1035a409a1300b463d95a5608a92e84
Gervel Giva <gervelgiva@gmail.com>
2022-07-30 07:50:28 +0800

    Exercise 2

---
+++ Gervel/MailService.Azure/AzureMailService.cs
@@ -0,0 +1,15 @@
+﻿using Core.Attributes;
+using Core.Contracts;
+
+[assembly: AutoGenerate]
+namespace MailService.Azure {
+
+	[Service(Contract = typeof(IMailServce))]
+	public class AzureMailService : IMailServce {
+		public string Name => "AzureMailService";
+
+		public string SendMail(string sender, string receiver, string subject, string body) {
+			return "From Azure Mail Service";
+		}
+	}
+}

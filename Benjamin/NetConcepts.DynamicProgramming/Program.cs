9c60375ab838eed005449a770f248059ff81fc6b
Benjamin Heredia <benjamin.heredia@kyocera.com>
2022-07-29 09:03:37 +0800

    Exercise

--- Benjamin/NetConcepts.DynamicProgramming/Program.cs
+++ Benjamin/NetConcepts.DynamicProgramming/Program.cs
@@ -20,43 +20,48 @@ namespace NetConcepts.DynamicProgramming
             //var companyType = typeof(CompanyService);
 
             var assembly = Assembly.LoadFile(@"C:\Training\Benjamin\NetConcepts.Model\bin\Debug\net5.0\NetConcepts.Model.dll");
-            foreach (var companyType in assembly.GetTypes())
-            {
-                Console.WriteLine($"Type of Employee {companyType.FullName}");
+            //foreach (var companyType in assembly.GetTypes())
+            //{
+            //    Console.WriteLine($"Type of Employee {companyType.FullName}");
 
-                // Instance Members
-                var companyFields = companyType.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+            //    // Instance Members
+            //    var companyFields = companyType.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
 
-                //var company = Activator.CreateInstance(typeof(Company));
-                //var c = Activator.CreateInstance(companyType, company);
-                Print(companyFields, "Fields");
-
-
-                var companyMethods = companyType.GetMethods(BindingFlags.Instance | BindingFlags.Public);
-                Print(companyMethods, "Methods");
-
-                var companyConst = companyType.GetConstructors(BindingFlags.Instance | BindingFlags.Public);
-                Print(companyConst, "Constructors");
+            //    //var company = Activator.CreateInstance(typeof(Company));
+            //    //var c = Activator.CreateInstance(companyType, company);
+            //    Print(companyFields, "Fields");
 
 
-                // Static Members
-                var staicFields = companyType.GetFields(BindingFlags.Static | BindingFlags.NonPublic);
-                Print(staicFields, "Static Fields");
+            //    var companyMethods = companyType.GetMethods(BindingFlags.Instance | BindingFlags.Public);
+            //    Print(companyMethods, "Methods");
 
-                // var attr = companyType.GetCustomAttribute(BindingFlags.Static | BindingFlags.NonPublic);
-                // MEF
-                // Managed Extensible Framework
-                // Plugin/ Plugout Architecure
-            }
+            //    var companyConst = companyType.GetConstructors(BindingFlags.Instance | BindingFlags.Public);
+            //    Print(companyConst, "Constructors");
+
+
+            //    // Static Members
+            //    var staicFields = companyType.GetFields(BindingFlags.Static | BindingFlags.NonPublic);
+            //    Print(staicFields, "Static Fields");
+
+
+            //    // MEF
+            //    // Managed Extensible Framework
+            //    // Plugin/ Plugout Architecure
+            //}
 
 
             // get all members, methods, properties, cons of training class
 
 
+            var trainingType = assembly.GetType("NetConcepts.Model.Models.Training");
+            var bindingFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic;
+            var members = trainingType.GetMembers(bindingFlags);
+            Print(members, "Members");
+            var methods = trainingType.GetMethods(bindingFlags);
+            Print(methods, "Methods");
 
-
-
-
+            var properties = trainingType.GetProperties(bindingFlags);
+            Print(properties, "Properties");
             Console.ReadLine();
         }
 

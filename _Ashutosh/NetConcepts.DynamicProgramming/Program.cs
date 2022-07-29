40e3736aca158c4f81bdf283c5a9219f0670dc74
Ashutosh Gupta <ashutosh.gupta@sciex.com>
2022-07-29 05:54:10 +0530

    Dynamic programming and reflection

--- _Ashutosh/NetConcepts.DynamicProgramming/Program.cs
+++ _Ashutosh/NetConcepts.DynamicProgramming/Program.cs
@@ -1,5 +1,4 @@
-﻿using NetConcepts.Model.Contracts;
-using System;
+﻿using System;
 using System.Linq;
 using System.Reflection;
 
@@ -9,10 +8,58 @@ namespace NetConcepts.DynamicProgramming
     {
         static void Main(string[] args)
         {
+            //var company = new Company();
+            //var empService = new EmployeeService(company);
+
+            //foreach (var emp in empService.GetEmployees())
+            //{
+            //    Console.WriteLine(emp.ToString());
+            //}
+
+            //var companyType = typeof(CompanyService);
+
+            var assembly = Assembly.LoadFile(@"C:\_Ashutosh\Trainings\AdvanceNetConcepts\_Ashutosh\NetConcepts.Model\bin\Debug\net5.0\NetConcepts.Model.dll");
+            foreach (var companyType in assembly.GetTypes())
+            {
+                Console.WriteLine($"Type of Employee {companyType.FullName}");
+
+                // Instance Members
+                var companyFields = companyType.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+                //var company = Activator.CreateInstance(typeof(Company));
+                //var c = Activator.CreateInstance(companyType, company);
+                Print(companyFields, "Fields");
+
+
+                var companyMethods = companyType.GetMethods(BindingFlags.Instance | BindingFlags.Public);
+                Print(companyMethods, "Methods");
+
+                var companyConst = companyType.GetConstructors(BindingFlags.Instance | BindingFlags.Public);
+                Print(companyConst, "Constructors");
+
+
+                // Static Members
+                var staicFields = companyType.GetFields(BindingFlags.Static | BindingFlags.NonPublic);
+                Print(staicFields, "Static Fields");
+
+
+                // MEF
+                // Managed Extensible Framework
+                // Plugin/ Plugout Architecure
+            }
 
             Console.ReadLine();
         }
 
-      
+        private static void Print(MemberInfo[] companyFields, string v)
+        {
+            Console.WriteLine(v);
+            foreach (var item in companyFields)
+            {
+                Console.WriteLine(item.Name);
+            }
+
+            Console.WriteLine("********");
+        }
     }
 }
---
+++ _Ashutosh/NetConcepts.Model/Models/Company.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetConcepts.Model.Models
+{
+    internal class Company
+    {
+        public IList<Employee> Employees;
+
+        public Company()
+        {
+            Employees = new List<Employee>();
+            Employees.Add(new Employee("First Emp", "1"));
+        }       
+    }
+}

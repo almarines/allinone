a55de4e462e385f4ba70d050450488cf291e55cb
Mike Higa <mike.higa@outlook.com>
2022-07-29 16:22:11 -0700

    push

--- Mike/NetConcepts.DynamicProgramming/Program.cs
+++ Mike/NetConcepts.DynamicProgramming/Program.cs
@@ -1,58 +1,48 @@
 ﻿using System;
-using System.Linq;
 using System.Reflection;
 
 namespace NetConcepts.DynamicProgramming {
   class Program {
     static void Main(string[] args) {
-      //var company = new Company();
-      //var empService = new EmployeeService(company);
-
-      //foreach (var emp in empService.GetEmployees())
-      //{
-      //    Console.WriteLine(emp.ToString());
-      //}
-
-      //var companyType = typeof(CompanyService);
-
-      var assembly = Assembly.LoadFile(@"C:\Temp\netcomm\AdvanceNetConcepts\Mike\NetConcepts.Model\bin\Debug\net5.0\NetConcepts.Model.dll");
-      //foreach(var companyType in assembly.GetTypes()) {
-      //  Console.WriteLine($"Type of Employee {companyType.FullName}");
-
-      //  // Instance Members
-      //  var companyFields = companyType.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
-
-      //  //var company = Activator.CreateInstance(typeof(Company));
-      //  //var c = Activator.CreateInstance(companyType, company);
-      //  Print(companyFields, "Fields");
-
-
-      //  var companyMethods = companyType.GetMethods(BindingFlags.Instance | BindingFlags.Public);
-      //  Print(companyMethods, "Methods");
-
-      //  var companyConst = companyType.GetConstructors(BindingFlags.Instance | BindingFlags.Public);
-      //  Print(companyConst, "Constructors");
-
-
-      //  // Static Members
-      //  var staicFields = companyType.GetFields(BindingFlags.Static | BindingFlags.NonPublic);
-      //  Print(staicFields, "Static Fields");
-
-
-      //  // MEF
-      //  // Managed Extensible Framework
-      //  // Plugin/ Plugout Architecure
-      //}
-
-      var companyServiceType = assembly.GetType("NetConcepts.Model.Contracts.CompanyService");
-
-      var companyMethods = companyServiceType.GetMethods(BindingFlags.Instance | BindingFlags.Public);
-      var companyInstanc = Activator.CreateInstance("NetConcepts.Model.Models.Company");
-      var companyServiceInstance = Activator.CreateInstance(companyServiceType, companyInstanc);
-      var getEmployees = companyMethods.FirstOrDefault(s => s.Name == "GetEmployees").Invoke(companyServiceInstance, null);
-
 
       // get all members, methods, properties, cons of training class
+
+      var assembly = Assembly.LoadFile(@"C:\Temp\netcomm\AdvanceNetConcepts\Mike\NetConcepts.Model\bin\Debug\net5.0\NetConcepts.Model.dll");
+      foreach(var companyType in assembly.GetTypes()) {
+        Console.WriteLine($"Type of Employee {companyType.FullName}");
+
+        // Instance Members
+        var companyFields = companyType.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+        //var company = Activator.CreateInstance(typeof(Company));
+        //var c = Activator.CreateInstance(companyType, company);
+        Print(companyFields, "Fields");
+
+        var companyMethods = companyType.GetMethods(BindingFlags.Instance | BindingFlags.Public);
+        Print(companyMethods, "Methods");
+
+        var companyConst = companyType.GetConstructors(BindingFlags.Instance | BindingFlags.Public);
+        Print(companyConst, "Constructors");
+
+        // Attributes Members
+        Console.WriteLine("****GetCustomAttributes*****");
+        var attributes = companyType.GetCustomAttributes();
+        foreach(var item in attributes) {
+          Console.WriteLine(item.GetType().FullName);
+        }
+
+        // Static Members
+        var staicFields = companyType.GetFields(BindingFlags.Static | BindingFlags.NonPublic);
+        Print(staicFields, "Static Fields");
+
+
+        //   MEF
+        //   Managed Extensible Framework
+        //   Plugin / Plugout Architecure
+      }
+
+
+      // 1st Assignment to Add new Employee using Reflection.
 
       Console.ReadLine();
     }

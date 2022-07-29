837561318e7fb808ec8b05f8dc42f383d552b549
Mike Higa <mike.higa@outlook.com>
2022-07-28 17:53:52 -0700

    dynamic programming

--- Mike/Main/Program.cs
+++ Mike/Main/Program.cs
@@ -1,14 +1,54 @@
 ﻿using System;
 
-namespace Main
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-          
+namespace Main {
+  class Program {
+    //private readonly Math mathObj;
 
-            Console.ReadKey();
-        }      
+
+    static void Main(string[] args) {
+      //Print(10);
+      //var mathObj = new Math();
+
+      //// Subscribe/ UnSubscribe
+      //mathObj.OnSumCalculated += (sender, e) => Console.WriteLine("Sum done");
+
+      //var result = mathObj.Sum(2, 2);
+      //Console.WriteLine(result);
+
+
+      //var stringContObj = new StringJoin();
+      //var result1 = stringContObj.Join("FirstName", "LastName");
+      //Console.WriteLine(result1);
+
+      //var myName = "Ashutosh";
+
+      Console.ReadKey();
     }
+
+    //private static void Print(int v)
+    //{
+    //    Console.WriteLine(v);
+    //}
+  }
+
+
+  //public class Math
+  //{
+  //    public event EventHandler OnSumCalculated;
+
+  //    public int Sum(int a, int b)
+  //    {
+  //        var result = a + b;
+  //        OnSumCalculated?.Invoke(this, null);
+
+  //        return result;
+  //    }
+  //}
+
+  //public class StringJoin
+  //{
+  //    public string Join(string a, string b) => string.Concat(a, b);
+  //}
+
+
 }

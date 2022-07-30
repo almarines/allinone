35d503d26bcf284fb0d721c3264bbfd26b69cf2a
John Mayordo <john.mayordo@kyocera.com>
2022-07-30 13:45:48 +0800

    push

--- John/NetConcepts.Model/Class1.cs
+++ John/NetConcepts.Model/Class1.cs
@@ -1,8 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace NetConcepts.Model
+namespace Core.Attributes
 {
-    public class Class1
+    /// <summary>
+    /// Attribute for setting assebmly for generation.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = false)]
+    public sealed class AutoGenerateAttribute : Attribute
     {
+        /// <summary>
+        /// Gets the name.
+        /// </summary>
+        public static string Name
+        {
+            get
+            {
+                return "AutoGenerate";
+            }
+        }
     }
 }

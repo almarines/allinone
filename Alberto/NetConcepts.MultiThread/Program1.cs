c3d782660e2e01b225a94b8556a517320e9439da
Alberto Moambing <alberto.moambing@kyocera.com>
2022-08-02 06:58:36 +0800

    my update

---
+++ Alberto/NetConcepts.MultiThread/Program1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NetConcepts.MultiThread
+{
+   public class Program1 
+    {
+        private static object lockObj = new object();
+
+        static void DisplayA()
+        {
+            lock (lockObj)
+            {
+                for(int i = 0; i < 10; i++)
+                {
+                    Thread.Sleep(1);
+                    Console.WriteLine("Display A");
+                }
+            }
+        }
+
+        static void DisplayB()
+        {
+            lock (lockObj)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Thread.Sleep(1);
+                    Console.WriteLine("Display B");
+                }
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            
+            //var obj = new Program();
+
+            //obj.DisplayA();
+            //obj.DisplayB();
+
+            Parallel.Invoke(() => DisplayA(), () => DisplayB());
+            Console.ReadKey();
+
+
+        }
+    }
+}

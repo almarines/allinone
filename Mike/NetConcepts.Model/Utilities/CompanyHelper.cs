194de92583f50d547141674ad2f23e9c62d11aab
Mike Higa <mike.higa@outlook.com>
2022-08-02 16:39:58 -0700

    AZ implementation for interface

---
+++ Mike/NetConcepts.Model/Utilities/CompanyHelper.cs
@@ -0,0 +1,17 @@
+﻿using NetConcepts.Model.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace NetConcepts.Model.Utilities {
+  internal static class CompanyHelper {
+    public static async Task<Company> CreateCompany(string companyName, int noOfEmp) {
+      var c = new Company(companyName);
+      for(int i = 1; i < noOfEmp; i++) {
+        c.Employees.Add(new Employee($"Emp {i}", i.ToString()));
+        Console.WriteLine($"got employee {i}");
+      }
+
+      return c;
+    }
+  }
+}
--- Mike/NetConcepts.MultiThread/Program.cs
+++ Mike/NetConcepts.MultiThread/Program.cs
@@ -1,12 +1,65 @@
-﻿using System;
+﻿using NetConcepts.Model.Models;
+using NetConcepts.Model.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace NetConcepts.MultiThread
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.ReadKey();
-        }
+namespace NetConcepts.MultiThread {
+  class Program {
+    static async Task Main(string[] args) {
+      Stopwatch stopwatch = new Stopwatch();
+      stopwatch.Start();
+
+      var result = await CompanyHelper.CreateCompany("test", 10);
+
+      //await Display1();
+
+      var list2 = GetEmployeeNames(result);
+      foreach(var emp in list2) {
+        Console.WriteLine(emp);
+      }
+
+      var list3 = GetNumbers();
+      foreach(var item in list3) {
+        Console.WriteLine(item);
+      }
+
+      stopwatch.Stop();
+      Console.WriteLine($"total time taken in Ms{stopwatch.ElapsedMilliseconds}");
+      Console.ReadKey();
     }
+
+    private static IEnumerable<int> GetNumbers() {
+      var list = new List<int>();
+      foreach(var item in Enumerable.Range(0, 10)) {
+        //list.Add(item);
+        yield return item;
+      }
+      //return list;
+    }
+
+    private static IEnumerable<string> GetEmployeeNames(Company c) {
+      foreach(var item in c.Employees) {
+        {
+          yield return item.Name;
+        }
+      }
+    }
+
+    private async static Task Display1() {
+      await Display2();
+      for(int i = 0; i < 100; i++) {
+        Console.WriteLine($"First Method {i}");
+      }
+    }
+
+    private static async Task Display2() {
+      for(int i = 0; i < 100; i++) {
+        await Task.Delay(1);
+        Console.WriteLine($"Second Method {i}");
+      }
+    }
+  }
 }

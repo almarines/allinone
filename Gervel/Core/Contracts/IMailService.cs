35d3b55a085c6c6a70ff45f94035c9f33224d99f
Ashutosh Gupta <ashutosh.gupta@sciex.com>
2022-08-14 16:44:46 +0530

    delete

--- Gervel/Core/Contracts/IMailService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Core.Contracts
-{
-    public interface IMailService : IService
-    {
-        string SendMail(string sender, string target, string subject, string body);
-    }
-}
--- Mike/NetConcepts.Model/Properties/CommonAssembly.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("NetConcepts.DynamicProgramming")]
-[assembly: InternalsVisibleTo("NetConcepts.MultiThread")]

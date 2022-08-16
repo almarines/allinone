edd93740037b78d01e9f6e7cfc584318f30abbe2
Elizabeth Paragoso <elizabeth.paragoso@kyocera.com>
2022-08-16 08:48:04 +0800

    Day1 - Design Patterns changes

--- Elizabeth/DesignPatterns/WebApi/Options/DbConfig.cs
+++ Elizabeth/DesignPatterns/WebApi/Options/DbConfig.cs
@@ -8,5 +8,18 @@ namespace WebApi.Options
     public class DbConfig
     {
         public string PathToDB { get; set; }
+
+        public Logging Logging { get; set; }
+    }
+    public class Logging
+    {
+        public LogLevel LogLevel { get; set; }
+    }
+
+    public class LogLevel
+    {
+        public string Default { get; set; }
+
+        public string Microsoft { get; set; }
     }
 }

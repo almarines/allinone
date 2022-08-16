aa7d39077054cff6da5a0d38c93ee2d55a76348d
Alberto Moambing <alberto.moambing@kyocera.com>
2022-08-17 04:39:35 +0800

    DesignPatternExercise1

--- Alberto/DesignPatterns/WebApi/Startup.cs
+++ Alberto/DesignPatterns/WebApi/Startup.cs
@@ -1,3 +1,7 @@
+using Customers.Domain.Repositories;
+using Customers.Infra.Options;
+using Customers.Infra.Repositories;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -7,12 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using WebApi.Options;
-using WebApi.Services;
+using System.Reflection;
 
 namespace WebApi
 {
@@ -28,18 +27,22 @@ namespace WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "LiteDB_Example", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "DP_Example", Version = "v1" });
             });
 
             services.Configure<DbConfig>(Configuration);
 
+            ////var section = Configuration.GetSection("Logging.LogLevel");
+            //services.Configure<Logging>(Configuration);
+            //var section = Configuration.GetSection("Logging").Get<Logging>();
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
             services.AddSingleton<ILiteDBContext, LiteDBContext>();
-            services.AddTransient<ILiteDBServices, LiteDBServices>();
-           
+            services.AddTransient<ICustomerRepository, CustomerRepository>();
+            services.AddTransient<ISupportRepository, SupportRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

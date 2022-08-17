9fc6f0d344dd8fa15bdd99a24133192aef16338f
Myla Rica <myla.rica@kyocera.com>
2022-08-18 06:34:54 +0800

    Strategy

---
+++ Myla/DesignPatterns/Creator_Struct_DB1/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Creator_Struct_DP1
+{
+
+    public interface ITrainingStrategy
+    {
+        int GetTrainignCost();
+
+        IEnumerable<string> GetCourses();
+
+    }
+
+    public class PythonTrainingStrategy : ITrainingStrategy
+    {
+        public int GetTrainignCost()
+        {
+            return 100;
+        }
+
+        public IEnumerable<string> GetCourses()
+        {
+            return new List<string> { "basic python", "py sql", "Dyngo" };
+        }
+    }
+ 
+    public class GoLangTrainingStrategy : ITrainingStrategy
+    {
+        public int GetTrainignCost()
+        {
+            return 150;
+        }
+
+        public IEnumerable<string> GetCourses()
+        {
+            return new List<string> { "basic Go" };
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var FullTimeEmp = new Employee(new FullTimeEmployeeStrategy());
+            var partTimeEmp = new Employee(new PartTimeEmployeeStrategy());
+
+            Console.WriteLine(FullTimeEmp.NumbersOfLeave());
+            Console.WriteLine(partTimeEmp.NumbersOfLeave());
+
+            Console.ReadKey();
+        }
+    }
+
+    public interface IEmployeeStrategy
+    {
+        int NumbersOfLeave();
+        string GetInsurance();
+        string Contract();
+    }
+
+    public class FullTimeEmployeeStrategy : IEmployeeStrategy
+    {
+        public string Contract()
+        {
+            return "Full Time contract";
+        }
+
+        public string GetInsurance()
+        {
+            return "XX";
+        }
+
+        public int NumbersOfLeave()
+        {
+            return 10;
+        }
+    }
+
+    public class PartTimeEmployeeStrategy : IEmployeeStrategy
+    {
+        public string Contract()
+        {
+            return "Part Time contract";
+        }
+
+        public string GetInsurance()
+        {
+            return "X";
+        }
+
+        public int NumbersOfLeave()
+        {
+            return 5;
+        }
+    }
+
+    public class Employee
+    {
+        private readonly IEmployeeStrategy employeeStrategy;
+
+        public Employee(IEmployeeStrategy employeeStrategy)
+        {
+            this.employeeStrategy = employeeStrategy;
+        }
+
+        public string Name { get; set; }
+
+        public int Id { get; set; }
+
+        public int NumbersOfLeave() => this.employeeStrategy.NumbersOfLeave();
+        public string GetInsurance() => this.employeeStrategy.GetInsurance();
+        public string Contract() => this.employeeStrategy.Contract();
+
+        // many more
+    }
+
+    //public class FullTimeEmployee : Emplpoyee
+    //{
+    //    public FullTimeEmployee()
+    //    {
+
+    //    }
+    //}
+
+    //public class PartTimeEmployee : Emplpoyee
+    //{
+
+    //}
+}
---
+++ Myla/DesignPatterns/Trainings.Api/Validators/GetCustomerIdQueryValidator.cs
@@ -0,0 +1,30 @@
+﻿using Trainings.Api.Application.Commands;
+using Trainings.Api.Application.Queries;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trainings.Api.Application.Validators
+{
+    public class GetTrainingIdQueryValidator : AbstractValidator<GetTrainingByIdQuery>
+    {
+        public GetTrainingIdQueryValidator()
+        {
+            RuleFor(query => query.Id).NotEmpty();
+        }
+    }
+
+    public class CreateTrainingCommandValidator : AbstractValidator<CreateTrainingCommand>
+    {
+        public CreateTrainingCommandValidator()
+        {
+            RuleFor(command => command.Id).NotEmpty().NotNull().WithMessage("Company Name length should be mor than 4");
+
+
+            RuleFor(c => c.Name).NotEmpty();
+
+        }
+    }
+}

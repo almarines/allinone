0afb92825899d225305b11a17e46325dd6d199f1
Gervel Giva <gervel.giva@kyocera.com>
2022-09-23 09:52:44 +0800

    Day 3 - Exercise - Completed all items

---
+++ Gervel/TDD/EmployeeManagementApi/Controllers/EmployeeController.cs
@@ -0,0 +1,50 @@
+﻿using EmployeeManagementApi.Dto;
+using EmployeeManagementApi.Models;
+using EmployeeManagementApi.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeRepository employeeRepository;
+
+        public EmployeeController(IEmployeeRepository employeeRepository)
+        {
+            this.employeeRepository = employeeRepository;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Employee>> GetAll()
+        {           
+            return await employeeRepository.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetEmployeeById(int Id)
+        {
+            var employee = await employeeRepository.Get(Id);
+			return Ok(employee);
+        }
+
+        [HttpPost]
+        public IActionResult InsertEmployee(EmployeeDto employeeDto)
+        {
+            if(string.IsNullOrEmpty(employeeDto.FirstName))
+            {
+                throw new InvalidOperationException();
+            }
+
+            var e = new Employee() { FirstName = employeeDto.FirstName, LastName = employeeDto.LastName };
+            var result = employeeRepository.InsertEmployee(e);
+
+            return Ok(result);
+        }
+    }
+}

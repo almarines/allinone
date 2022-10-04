0a66afb0bf32a08241e0166f1f4824df6edf6cfa
Ashutosh Gupta <ashutosh.gupta@sciex.com>
2022-10-05 05:17:21 +0530

    employeedbcontext

---
+++ _Ashutosh/SOLID/SRP_DI/EmployeeManagementApi/Managers/IEmployeeRepository.cs
@@ -0,0 +1,29 @@
+﻿using EmployeeManagementApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementApi.Managers
+{
+    public interface IEmployeeRepository
+    {
+        Task<int> InsertEmployee(Employee employee);
+    }
+
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly EmployeeDBContext employeeDBContext;
+
+        public EmployeeRepository(EmployeeDBContext employeeDBContext)
+        {
+            this.employeeDBContext = employeeDBContext;
+        }
+
+        public async Task<int> InsertEmployee(Employee employee)
+        {
+           employeeDBContext.Employees.Add(employee);
+           return await employeeDBContext.SaveChangesAsync();
+        }
+    }
+}

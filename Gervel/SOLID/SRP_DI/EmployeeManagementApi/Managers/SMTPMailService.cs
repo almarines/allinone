9518f0add9ff1ec7cd8a1eb0ca30f4a19c64a3ed
Gervel Giva <gervel.giva@kyocera.com>
2022-10-05 09:00:45 +0800

    SOLID - Day 1 Exercise 1 (Insert and Get All Employee)

--- Gervel/SOLID/SRP_DI/EmployeeManagementApi/Managers/SMTPMailService.cs
+++ Gervel/SOLID/SRP_DI/EmployeeManagementApi/Managers/SMTPMailService.cs
@@ -5,39 +5,43 @@ using System.Net;
 using System.Net.Mail;
 using System.Threading.Tasks;
 
-namespace EmployeeManagementApi.Managers
-{
-    public class SMTPMailService
-    {
-        public async Task<bool> SendMail(string to, string subject, string body)
-        {
-            try
-            {
-                SmtpClient client = new SmtpClient();
-                client.Host = "<host>";
-                client.Port = int.MaxValue;
+namespace EmployeeManagementApi.Managers {
+	public class SMTPMailService {
+		public async Task<bool> SendMail(string to, string subject, string body) {
+			try {
+				SmtpClient client = new SmtpClient();
+				client.Host = "<host>";
+				client.Port = int.MaxValue;
 
-                client.UseDefaultCredentials = false;
-                client.Credentials = new NetworkCredential("<username>", "<password>");
+				client.UseDefaultCredentials = false;
+				client.Credentials = new NetworkCredential("<username>", "<password>");
 
-                MailMessage message = CreateMailMessage(to, subject, body);
-                client.Send(message);
-                return await Task.FromResult(true);
-            }
-            catch (Exception)
-            {
-                return false;
-            }
-        }
+				MailMessage message = CreateMailMessage(to, subject, body);
+				client.Send(message);
+				return await Task.FromResult(true);
+			} catch (Exception) {
+				return false;
+			}
+		}
 
-        private MailMessage CreateMailMessage(string to, string subject, string body)
-        {
-            MailMessage mailMessage = new MailMessage();
-            mailMessage.From = new MailAddress("<registered mail id>");
-            mailMessage.To.Add(to);
-            mailMessage.Body = body;
-            mailMessage.Subject = subject;
-            return mailMessage;
-        }
-    }
+		private MailMessage CreateMailMessage(string to, string subject, string body) {
+			MailMessage mailMessage = new MailMessage();
+			mailMessage.From = new MailAddress("<registered mail id>");
+			mailMessage.To.Add(to);
+			mailMessage.Body = body;
+			mailMessage.Subject = subject;
+			return mailMessage;
+		}
+
+		public bool IsValid(string value) {
+
+			if (string.IsNullOrEmpty(value)) { return false; }
+
+			if (!value.Contains("@")) { return false; }
+
+			return true;
+		}
+
+
+	}
 }

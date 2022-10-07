e31fbf6ceb38f52a7d12c243de4cd79f29f19047
Gervel Giva <gervel.giva@kyocera.com>
2022-10-08 06:04:11 +0800

    SOLID - Day 4 - Exercise 1 - Completed

---
+++ Gervel/SOLID/SRP_DI/MongoDBServer/DBContext/EmployeeDBContext.cs
@@ -0,0 +1,35 @@
+﻿using Core;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using System;
+
+namespace DatabaseCore {
+	public class EmployeeDBContext {
+		private readonly IOptions<DbConfig> _dbOptions;
+
+
+		public EmployeeDBContext(IOptions<DbConfig> dbOptions) {
+			_dbOptions = dbOptions;
+
+			InitializeDatabase();
+		}
+
+		public IMongoCollection<Employee> Employees { get; private set; }
+
+		private void InitializeDatabase() {
+			try {
+				var mongoDBConfig = _dbOptions.Value.PathToDB;
+
+				var databaseName = MongoUrl.Create(mongoDBConfig)?.DatabaseName;
+				var dbClient = new MongoClient(mongoDBConfig);
+				var db = dbClient.GetDatabase(databaseName);
+
+				Employees = db.GetCollection<Employee>(nameof(Employee));
+
+			} catch {
+				throw new Exception("Invalid mongo configuration or Connection string");
+			}
+		}
+
+	}
+}
---
+++ Gervel/SOLID/SRP_DI/MongoDBServer/EmployeeRepository.cs
@@ -0,0 +1,54 @@
+﻿using Core;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace DatabaseCore {
+	public class EmployeeRepository : IEmployeeRepository {
+		private readonly EmployeeDBContext employeeDBContext;
+		private readonly IMailService mailService;
+
+		public EmployeeRepository(EmployeeDBContext employeeDBContext, IMailService mailService) {
+			this.employeeDBContext = employeeDBContext;
+			this.mailService = mailService;
+		}
+
+		public async Task<IEnumerable<Employee>> GetAll() {
+			var result = await employeeDBContext.Employees.FindAsync(_ => true);
+			return result.ToList();
+
+		}
+
+		public async Task<Employee> GetById(int id) {
+			FilterDefinition<Employee> filter = Builders<Employee>.Filter.Eq(m => m.Id, id);
+			IAsyncCursor<Employee> results = await employeeDBContext.Employees.FindAsync(filter);
+			return await results.FirstOrDefaultAsync();
+		}
+
+		public async Task<Employee> GetByName(string name) {
+			FilterDefinition<Employee> filter = Builders<Employee>.Filter.Eq(m => m.FirstName, name);
+			IAsyncCursor<Employee> results = await employeeDBContext.Employees.FindAsync(filter);
+			return await results.FirstOrDefaultAsync();
+		}
+
+		public async Task<int> GetSalary(int id) {
+			var emp = await GetById(id);
+			return emp.GetSalary();
+		}
+
+		public async Task<string> GetInsurance(int id) {
+			var emp = await GetById(id);
+			return emp.GetInsurance();
+		}
+
+		public async Task<int> InsertEmployee(Employee employee) {
+			try {
+				await employeeDBContext.Employees.InsertOneAsync(employee);
+				return 1;
+			} catch {
+				return 0;
+			}
+		}
+	}
+}
